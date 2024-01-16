25cad0a80843a36faa70b8280d4562fda834b1de
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-16 13:55:45 +0530

    Code Refactoring

--- TicketManagement/src/Core/TicketManagement.Domain/Entities/Category.cs
+++ TicketManagement/src/Core/TicketManagement.Domain/Entities/Category.cs
@@ -1,11 +1,12 @@
 ﻿using TicketManagement.Domain.Common;
 
-namespace GloboTicket.TicketManagement.Domain.Entities
+namespace TicketManagement.Domain.Entities;
+
+public class Category : AuditableEntity
 {
-    public class Category: AuditableEntity
-    {
-        public Guid CategoryId { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public ICollection<Event>? Events { get; set; }
-    }
+    public Guid CategoryId { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<Event>? Events { get; set; }
 }
--- TicketManagement/src/Core/TicketManagement.Domain/Entities/Order.cs
+++ TicketManagement/src/Core/TicketManagement.Domain/Entities/Order.cs
@@ -8,11 +8,11 @@ namespace TicketManagement.Domain.Entities
         public Guid Id { get; set; }
 
         public Guid UserId { get; set; }
-        
+
         public int OrderTotal { get; set; }
-        
+
         public DateTime OrderPlaced { get; set; }
-        
+
         public bool OrderPaid { get; set; }
     }
 
