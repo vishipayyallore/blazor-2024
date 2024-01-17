c24811dfc306057ff31790d52db84866dda1defa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-17 13:41:14 +0530

    Update Order.cs

--- TicketManagement/src/Core/TicketManagement.Domain/Entities/Order.cs
+++ TicketManagement/src/Core/TicketManagement.Domain/Entities/Order.cs
@@ -1,19 +1,16 @@
 ﻿using TicketManagement.Domain.Common;
 
-namespace TicketManagement.Domain.Entities
+namespace TicketManagement.Domain.Entities;
+
+public class Order : AuditableEntity
 {
+    public Guid Id { get; set; }
 
-    public class Order : AuditableEntity
-    {
-        public Guid Id { get; set; }
+    public Guid UserId { get; set; }
 
-        public Guid UserId { get; set; }
+    public int OrderTotal { get; set; }
 
-        public int OrderTotal { get; set; }
+    public DateTime OrderPlaced { get; set; }
 
-        public DateTime OrderPlaced { get; set; }
-
-        public bool OrderPaid { get; set; }
-    }
-
+    public bool OrderPaid { get; set; }
 }
