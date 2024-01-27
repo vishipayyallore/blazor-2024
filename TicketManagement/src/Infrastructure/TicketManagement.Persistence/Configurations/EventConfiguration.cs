a3c3134f147139c746cd961df0a40f7cb9beeea7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-27 16:42:54 +0530

    Code

---
+++ TicketManagement/src/Infrastructure/TicketManagement.Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TicketManagement.Persistence.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+
+        }
+    }
+}
---
+++ TicketManagement/src/Infrastructure/TicketManagement.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace TicketManagement.Persistence.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+        public EventRepository(GloboTicketDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+        {
+            var matches = _dbContext.Events.Any(e => e.Name.Equals(name) && e.Date.Date.Equals(eventDate.Date));
+            return Task.FromResult(matches);
+        }
+    }
+}
