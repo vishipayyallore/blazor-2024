e6c5c79d7b59b74de10fc32cee2b38ee086cafaa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-27 17:03:35 +0530

    Email

---
+++ TicketManagement/src/Core/TicketManagement.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using TicketManagement.Application.Models.Mail;
+
+namespace TicketManagement.Application.Contracts.Infrastructure;
+
+public interface IEmailService
+{
+    Task<bool> SendEmail(Email email);
+}
---
+++ TicketManagement/src/Core/TicketManagement.Application/Models/Mail/EmailSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketManagement.Application.Models.Mail;
+
+public class EmailSettings
+{
+    public string ApiKey { get; set; } = string.Empty;
+
+    public string FromAddress { get; set; } = string.Empty;
+
+    public string FromName { get; set; } = string.Empty;
+}
