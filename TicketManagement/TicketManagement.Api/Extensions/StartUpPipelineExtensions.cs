f489039e4e19ffae218eeb533fead9d4efd034a1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-30 17:58:49 +0530

    Code Refactoring

---
+++ TicketManagement/TicketManagement.Api/Extensions/StartUpPipelineExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace TicketManagement.Api.Extensions
+{
+    internal static class StartUpPipelineExtensions
+    {
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+
+            _ = app.UseCors("open");
+
+            if (app.Environment.IsDevelopment())
+            {
+                _ = app.UseSwagger();
+                _ = app.UseSwaggerUI();
+            }
+
+            _ = app.UseHttpsRedirection();
+            _ = app.MapControllers();
+
+            return app;
+        }
+    }
+}
