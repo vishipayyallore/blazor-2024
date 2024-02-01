3cf7e7861547e864a8509bec12f24c59cca77436
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-01 16:32:37 +0530

    Update Program.cs

--- TicketManagement/TicketManagement.Api/Program.cs
+++ TicketManagement/TicketManagement.Api/Program.cs
@@ -1,27 +1,11 @@
 using TicketManagement.Api.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var app = builder
+WebApplication app = builder
        .ConfigureServices()
        .ConfigurePipeline();
 
 await app.ResetDatabaseAsync();
 
 app.Run();
-
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
