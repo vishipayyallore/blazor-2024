96926aa50bdfb01c2987b0a5674b84f5af08d934
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-01 17:00:25 +0530

    New Controller

--- TicketManagement/TicketManagement.Api/Controllers/CategoryController.cs
+++ TicketManagement/TicketManagement.Api/Controllers/CategoryController.cs
@@ -1,5 +1,4 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TicketManagement.Application.Features.Categories.Commands.CreateCategory;
 using TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
---
+++ TicketManagement/TicketManagement.Api/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TicketManagement.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+}
